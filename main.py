d5b1806ff13a763ac453b5cba100c5b485d1d98d
Emma Yang <emmayang78@gmail.com>
2024-12-10 23:54:32 -0500

    Add score reset warning message

--- main.py
+++ main.py
@@ -210,6 +210,8 @@ while running:
                     courses_enrolled = False
                     week += 1
                     day = "MONDAY"
+                    if week == 11:
+                        displaying_warning = 4
                     reset_category()
 
         elif week <= 20:
@@ -220,17 +222,11 @@ while running:
                 draw_interactive_selection(window, options, current_course_selection)
                 # Show status if threshold exceeded
                 if displaying_warning > 0:
-                    warning_key = list(category_scores.keys())[displaying_warning - 1]
-                    if (
-                        category_scores[warning_key] < 0
-                        and category_warning_count[warning_key] == 0
-                    ) or (
-                        category_scores[warning_key] < -5
-                        and category_warning_count[warning_key] == 1
-                    ):
+                    if displaying_warning == 4:
+                        # special case for score reset
                         draw_warning(
                             window,
-                            warnings[warning_key][category_warning_count[warning_key]],
+                            "You've taken a break to reset and starting your semester fresh and ready to go! Your scores have been reset.",
                             "GOT IT",
                         )
                         for event in pygame.event.get():
@@ -240,34 +236,62 @@ while running:
                                     or event.key == pygame.K_SPACE
                                     or event.key == pygame.K_KP_ENTER
                                 ):
-                                    category_warning_count[warning_key] += 1
-                                    displaying_warning -= 1
-                    elif (
-                        category_scores[warning_key] <= -10
-                        and category_warning_count[warning_key] == 2
-                    ):
-                        draw_warning(
-                            window,
-                            warnings[warning_key][category_warning_count[warning_key]],
-                            "GAME OVER",
-                        )
-                        for event in pygame.event.get():
-                            if event.type == pygame.KEYDOWN:
-                                if (
-                                    event.key == pygame.K_RETURN
-                                    or event.key == pygame.K_SPACE
-                                    or event.key == pygame.K_KP_ENTER
-                                ):
-                                    current_state = GameState.INIT_0
-                                    reset()
+                                    displaying_warning = 0
                     else:
-                        displaying_warning -= 1
+                        warning_key = list(category_scores.keys())[
+                            displaying_warning - 1
+                        ]
+                        if (
+                            category_scores[warning_key] < 0
+                            and category_warning_count[warning_key] == 0
+                        ) or (
+                            category_scores[warning_key] < -5
+                            and category_warning_count[warning_key] == 1
+                        ):
+                            draw_warning(
+                                window,
+                                warnings[warning_key][
+                                    category_warning_count[warning_key]
+                                ],
+                                "GOT IT",
+                            )
+                            for event in pygame.event.get():
+                                if event.type == pygame.KEYDOWN:
+                                    if (
+                                        event.key == pygame.K_RETURN
+                                        or event.key == pygame.K_SPACE
+                                        or event.key == pygame.K_KP_ENTER
+                                    ):
+                                        category_warning_count[warning_key] += 1
+                                        displaying_warning -= 1
+                        elif (
+                            category_scores[warning_key] <= -10
+                            and category_warning_count[warning_key] == 2
+                        ):
+                            draw_warning(
+                                window,
+                                warnings[warning_key][
+                                    category_warning_count[warning_key]
+                                ],
+                                "GAME OVER",
+                            )
+                            for event in pygame.event.get():
+                                if event.type == pygame.KEYDOWN:
+                                    if (
+                                        event.key == pygame.K_RETURN
+                                        or event.key == pygame.K_SPACE
+                                        or event.key == pygame.K_KP_ENTER
+                                    ):
+                                        current_state = GameState.INIT_0
+                                        reset()
+                        else:
+                            displaying_warning -= 1
 
-                    if displaying_warning == 0:
-                        # Transition to the next randomization
-                        screen_idx += 1
-                        current_course_selection = []
-                        random_event = random.choice(random_options)
+                        if displaying_warning == 0:
+                            # Transition to the next randomization
+                            screen_idx += 1
+                            current_course_selection = []
+                            random_event = random.choice(random_options)
                 else:
                     random_event = random.choice(random_options)
                 done = get_keyboard_selection(current_course_selection, True, 2)
